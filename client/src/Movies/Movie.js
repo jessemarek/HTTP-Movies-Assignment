0a8e3b6e9e974940b606126b62453e24b7163bb5
unknown <dethgrenade@gmail.com>
2020-05-20 12:43:12 -0700

    update form and edit button working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory()
 
   const fetchMovie = (id) => {
     axios
@@ -29,6 +30,10 @@ function Movie({ addToSavedList }) {
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
+
+      <div className="edit-button" onClick={() => push(`/update-movie/${params.id}`)}>
+        Edit
+      </div>
 
       <div className="save-button" onClick={saveMovie}>
         Save
